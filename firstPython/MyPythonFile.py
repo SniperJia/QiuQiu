d39d89539d9c03c43188ee7941d70038ec37d714
SniperJia <Sniper_Jia@126.com>
2019-03-07 23:30:06 +0800

    first commit

---
+++ firstPython/MyPythonFile.py
@@ -0,0 +1,14 @@
+#!/usr/bin python
+#encode:utf-8
+
+def fristpython():
+    i = 10
+    for i in (1,100):
+        if i%2 == 0:
+            print "Hello World"
+            i = i+1
+        else :
+            print "Bad"
+            i = i + 1
+
+fristpython()
